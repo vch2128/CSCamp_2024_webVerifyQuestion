692ca02d5ca8a7c66e62d16477513e470742ebd5
vitolin0416 <vitolin0416@gmail.com>
2024-06-28 13:09:40 +0800

    change square_series.py to series.py

--- echo_square.py
+++ echo_square.py
@@ -1,5 +1,5 @@
 
-def solve_task1(number):   # star_advance
+def solve_task1(number):   # echo_square
     return number**2
 x = int(input())
 a = solve_task1(x)
--- square_series.py
+++
@@ -1,7 +0,0 @@
-
-def solve_task1(number):   # square_series
-    
-    return (number)*(number+1)*(number*2+1)/6
-
-x = int(input())
-print(solve_task1(x))
